05eea979a8fa7bb6418b8723aa048387bbf1e16f
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-12-01 17:05:40 +0100

    fix email

--- yummy/server/helpers/sendEmail.js
+++ yummy/server/helpers/sendEmail.js
@@ -8,7 +8,7 @@ const { SENDGRID_KEY } = process.env;
 sgMail.setApiKey(SENDGRID_KEY);
 
 const sendEmail = async (data) => {
-  const email = { ...data, from: "zinich911@gmail.com" };
+  const email = { ...data, from: "digiorderbusiness@gmail.com" };
 
   await sgMail.send(email);
 
